1e8f5667dcf7f37e100382ff6c058af5636bb789
jovianka <jovianpratama123@gmail.com>
2023-09-29 20:10:21 +0800

    remove unneeded packages (there are still orphaned packages tho, hmm)

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import streamlit as st
-import altair as alt
 from streamlit_gsheets import GSheetsConnection
+import altair as alt
 import requests
-import pygsheets
-
-
-st.header("Jabodetabek Weather Forecast!")
+import pandas as pd
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-client = pygsheets.authorize(service_account_file=".streamlit/unique-voyage-354204-5359857be23a.json")
-sheets = client.open('sickathon-weather')
-wks = sheets.sheet1
+st.header("Jabodetabek Weather Forecast!")
+
 
 locations = {
     "Jakarta Pusat": (-8.7909, 115),
@@ -19,11 +15,11 @@ locations = {
     "Depok": (-6.4, 106.8186),
     "Tangerang": (-6.1781, 106.63),
     "Bekasi": (-6.2349, 106.9896),
+    "Jimbaran": (-8.7909, 115.1601)
 }
 
-location = "Jakarta Pusat"
+location = st.selectbox("Please pick your location", ("Jakarta Pusat", "Bogor", "Depok", "Tangerang", "Bekasi", "Jimbaran"), index=0, placeholder="--Location--")
 
-location = st.selectbox("Please pick your location", ("Jakarta Pusat", "Bogor", "Depok", "Tangerang", "Bekasi"), index=0, placeholder="--Location--")
 
 def get_chart(df, weather_variable):
     hover = alt.selection_point(
@@ -111,7 +107,6 @@ def get_chart(df, weather_variable):
         ).add_params(hover)
         )
     
-    
     if weather_variable == "temperature":
         return (lines_temperature + points_temperature + tooltips_temperature).interactive()
     elif weather_variable == "dewpoint":
@@ -119,31 +114,34 @@ def get_chart(df, weather_variable):
     elif weather_variable == "apparent_temperature":
         return (lines_apparent_temperature + points_apparent_temperature + tooltips_apparent_temperature).interactive()
 
+
 if st.button("Predict!"):
     data = requests.get("https://api.open-meteo.com/v1/forecast", params={ "latitude": locations[location][0], \
                                                                         "longitude": locations[location][1], \
                                                                         "timezone": "Asia/Singapore", \
                                                                         "hourly": ["temperature_2m", "dewpoint_2m", "apparent_temperature"]}).json()
 
-    wks.update_col(1, [data["latitude"]], 1)
-    wks.update_col(2, [data["longitude"]], 1)
-    wks.update_col(3, [data["elevation"]], 1)
-    wks.update_col(4, [data["utc_offset_seconds"]], 1)
-    wks.update_col(5, [data["timezone"]], 1)
-    wks.update_col(6, [data["timezone_abbreviation"]], 1)
-    wks.update_col(7, [data["hourly"]["time"]], 1)
-    wks.update_col(8, [data["hourly"]["temperature_2m"]], 1)
-    wks.update_col(9, [data["hourly"]["dewpoint_2m"]], 1)
-    wks.update_col(10, [data["hourly"]["apparent_temperature"]], 1)
+    data_df = pd.DataFrame({
+        "time": data["hourly"]["time"],
+        "temperature_2m": data["hourly"]["temperature_2m"],
+        "dewpoint_2m": data["hourly"]["dewpoint_2m"],
+        "apparent_temperature": data["hourly"]["apparent_temperature"]
+    })
 
+    conn.update(spreadsheet=st.secrets.connections.gsheets.spreadsheet,
+                worksheet=st.secrets.connections.gsheets.worksheet,
+                data=pd.DataFrame.from_dict(data_df, orient="columns"))
+
+    conn.reset()
     df = conn.read(spreadsheet=st.secrets.connections.gsheets.spreadsheet, worksheet=st.secrets.connections.gsheets.worksheet)
-    
+
+
     chart_temperature = get_chart(df, "temperature")
     chart_dewpoint = get_chart(df, "dewpoint")
     chart_apparent_temperature = get_chart(df, "apparent_temperature")
 
-    st.header(f"How hot is it in {location}?")
 
+    st.header(f"How hot is it in {location}?")
     st.altair_chart(chart_temperature, use_container_width=True)
     st.altair_chart(chart_dewpoint, use_container_width=True)
     st.altair_chart(chart_apparent_temperature, use_container_width=True)
