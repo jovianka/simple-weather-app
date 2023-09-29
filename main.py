2cc425c7e6a09e5750483258d87b1d03ee27ca0c
jovianka <jovianpratama123@gmail.com>
2023-09-29 11:46:56 +0800

    indent

--- main.py
+++ main.py
@@ -29,101 +29,101 @@ df = conn.read(spreadsheet=st.secrets.connections.gsheets.spreadsheet, worksheet
 # df = conn.read(url="https://docs.google.com/spreadsheets/d/1KQ_vqbycPrnZGtn2Ylk_cT8bpCkzsmf2KklJasKr9K4/edit?usp=sharing")
 
 def get_chart(df, weather_variable):
-  hover = alt.selection_point(
-      fields=["time"],
-      nearest=True,
-      on="mouseover",
-      empty=True,
-    )
-  
-  # Temperature
-  lines_temperature = (
-      alt.Chart(df, title="Temperature Forecast")
-      .mark_line()
-      .encode(
-          x=alt.X("time:T"),
-          y="temperature_2m",
+    hover = alt.selection_point(
+        fields=["time"],
+        nearest=True,
+        on="mouseover",
+        empty=True,
         )
-      )
-
-  points_temperature = lines_temperature.transform_filter(hover).mark_circle(size=65)
-
-  tooltips_temperature = (
-      alt.Chart(df)
-      .mark_rule()
-      .encode(
-          x=alt.X("time:T").title("Date").axis(format="%d - %B"),
-          y=alt.Y("temperature_2m").title("Temperature (°C)"),
-          opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-          tooltip=[
-            alt.Tooltip("time:T", title="Time", format="%a, %d %B %Y %H:%M"),
-            alt.Tooltip("temperature_2m", title="Temperature"),
-          ],
-      ).add_params(hover)
-    )
-
-  # Dewpoint
-  lines_dewpoint = (
-      alt.Chart(df, title="Dewpoint Forecast")
-      .mark_line()
-      .encode(
-          x=alt.X("time:T"),
-          y="dewpoint_2m",
+    
+    # Temperature
+    lines_temperature = (
+        alt.Chart(df, title="Temperature Forecast")
+        .mark_line()
+        .encode(
+            x=alt.X("time:T"),
+            y="temperature_2m",
+            )
         )
-      )
 
-  points_dewpoint = lines_dewpoint.transform_filter(hover).mark_circle(size=65)
+    points_temperature = lines_temperature.transform_filter(hover).mark_circle(size=65)
 
-  tooltips_dewpoint = (
-      alt.Chart(df)
-      .mark_rule()
-      .encode(
-          x=alt.X("time:T").title("Date").axis(format="%d - %B"),
-          y=alt.Y("dewpoint_2m").title("Dewpoint (°C)"),
-          opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-          tooltip=[
-            alt.Tooltip("time:T", title="Time", format="%a, %d %B %Y %H:%M"),
-            alt.Tooltip("dewpoint_2m", title="Dewpoint"),
-          ],
-      ).add_params(hover)
-    )
-
-  # Apparent Temperature
-  lines_apparent_temperature = (
-      alt.Chart(df, title="Apparent Temperature Forecast")
-      .mark_line()
-      .encode(
-          x=alt.X("time:T"),
-          y="apparent_temperature",
+    tooltips_temperature = (
+        alt.Chart(df)
+        .mark_rule()
+        .encode(
+            x=alt.X("time:T").title("Date").axis(format="%d - %B"),
+            y=alt.Y("temperature_2m").title("Temperature (°C)"),
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("time:T", title="Time", format="%a, %d %B %Y %H:%M"),
+                alt.Tooltip("temperature_2m", title="Temperature"),
+            ],
+        ).add_params(hover)
         )
-      )
 
-  points_apparent_temperature = lines_apparent_temperature.transform_filter(hover).mark_circle(size=65)
+    # Dewpoint
+    lines_dewpoint = (
+        alt.Chart(df, title="Dewpoint Forecast")
+        .mark_line()
+        .encode(
+            x=alt.X("time:T"),
+            y="dewpoint_2m",
+            )
+        )
 
-  tooltips_apparent_temperature = (
-      alt.Chart(df)
-      .mark_rule()
-      .encode(
-          x=alt.X("time:T").title("Date").axis(format="%d - %B"),
-          y=alt.Y("apparent_temperature").title("Apparent Temperature (°C)"),
-          opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-          tooltip=[
-            alt.Tooltip("time:T", title="Time", format="%a, %d %B %Y %H:%M"),
-            alt.Tooltip("apparent_temperature", title="Apparent Temperature"),
-          ],
-      ).add_params(hover)
-    )
-  
-  
-  match weather_variable:
-      case "temperature":
-          return (lines_temperature + points_temperature + tooltips_temperature).interactive()
+    points_dewpoint = lines_dewpoint.transform_filter(hover).mark_circle(size=65)
 
-      case "dewpoint":
-          return (lines_dewpoint + points_dewpoint + tooltips_dewpoint).interactive()
+    tooltips_dewpoint = (
+        alt.Chart(df)
+        .mark_rule()
+        .encode(
+            x=alt.X("time:T").title("Date").axis(format="%d - %B"),
+            y=alt.Y("dewpoint_2m").title("Dewpoint (°C)"),
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("time:T", title="Time", format="%a, %d %B %Y %H:%M"),
+                alt.Tooltip("dewpoint_2m", title="Dewpoint"),
+            ],
+        ).add_params(hover)
+        )
 
-      case "apparent_temperature":
-          return (lines_apparent_temperature + points_apparent_temperature + tooltips_apparent_temperature).interactive()
+    # Apparent Temperature
+    lines_apparent_temperature = (
+        alt.Chart(df, title="Apparent Temperature Forecast")
+        .mark_line()
+        .encode(
+            x=alt.X("time:T"),
+            y="apparent_temperature",
+            )
+        )
+
+    points_apparent_temperature = lines_apparent_temperature.transform_filter(hover).mark_circle(size=65)
+
+    tooltips_apparent_temperature = (
+        alt.Chart(df)
+        .mark_rule()
+        .encode(
+            x=alt.X("time:T").title("Date").axis(format="%d - %B"),
+            y=alt.Y("apparent_temperature").title("Apparent Temperature (°C)"),
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("time:T", title="Time", format="%a, %d %B %Y %H:%M"),
+                alt.Tooltip("apparent_temperature", title="Apparent Temperature"),
+            ],
+        ).add_params(hover)
+        )
+    
+    
+    match weather_variable:
+        case "temperature":
+            return (lines_temperature + points_temperature + tooltips_temperature).interactive()
+
+        case "dewpoint":
+            return (lines_dewpoint + points_dewpoint + tooltips_dewpoint).interactive()
+
+        case "apparent_temperature":
+            return (lines_apparent_temperature + points_apparent_temperature + tooltips_apparent_temperature).interactive()
 
 chart_temperature = get_chart(df, "temperature")
 chart_dewpoint = get_chart(df, "dewpoint")
